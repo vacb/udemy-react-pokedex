73639901772437aa7eef80ff197ec997bb71fd7e
Victoria Blackburn <v.a.c.blackburn@gmail.com>
2021-11-17 14:31:37 +0000

    Game component styling with conditional on winning or losing hand

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -16,12 +16,17 @@ class Pokedex extends Component {
     ],
   };
   render() {
+    let title;
+    if (this.props.isWinner) {
+      title = <h1 className="Pokedex-winner">Winning hand</h1>;
+    } else {
+      title = <h1 className="Pokedex-loser">Losing hand</h1>;
+    }
+
     return (
       <div className="Pokedex">
-        <h1>Pokedex!</h1>
-        <p>Total experience: {this.props.exp}</p>
-
-        <p>{this.props.isWinner ? "Winner" : "Loser"}</p>
+        {title}
+        <h4>Total experience: {this.props.exp}</h4>
         <div className="Pokedex-cards">
           {this.props.pokemon.map((p) => (
             <Pokecard
